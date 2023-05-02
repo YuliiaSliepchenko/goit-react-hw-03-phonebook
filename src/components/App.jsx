46d59a0e9193d4f5d2bcdb008db4674b6baaac0d
YuliiaSliepchenko <115469039+YuliiaSliepchenko@users.noreply.github.com>
2023-05-02 19:09:55 +0300

    t1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,13 +10,21 @@ import s from './App.module.css'
 export class App extends Component {
   state = {
      contacts: [
-     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    //  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
      ],
      filter: '',
 };
+
+  componentDidMount() {
+    const contactStorige = JSON.parse(localStorage.getItem('contacts'));
+    if (contactStorige) this.setState({ contacts: contactStorige });
+  }
+  componentDidUpdate(_, prevState) {
+   if(this.state.contacts !== prevState.contacts) localStorage.setItem('contacts',JSON.stringify(this.state.contacts))
+  }
 
 contactAdd = ({ name, number }) => {
       const { contacts } = this.state;
